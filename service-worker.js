aefee5e064942937ccafde84515c495766f1fe8e
webqueretaro <arcosqueretaro@gmail.com>
2025-04-13 13:55:30 -0600

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,38 +1,24 @@
-const CACHE_NAME = 'asteroids-tetris-v5';
-const urlsToCache = [
-    '/juego/',
-    '/juego/index.html',
-    '/juego/press-start-2p.woff2',
-    '/juego/icon.png',
-    '/juego/manifest.json'
-];
-
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(urlsToCache))
-            .catch(err => console.log('Error en install:', err))
-    );
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('cometa-cache-v1').then((cache) => {
+      return cache.addAll([
+        '/cometa/',
+        '/cometa/index.html',
+        '/cometa/manifest.json',
+        '/cometa/icons/icon-192.png',
+        '/cometa/icons/icon-512.png',
+        '/cometa/images/imagen1.jpg',
+        '/cometa/images/imagen2.jpg',
+        '/cometa/images/imagen3.jpg'
+      ]);
+    })
+  );
 });
 
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => response || fetch(event.request))
-    );
-});
-
-self.addEventListener('activate', event => {
-    const cacheWhitelist = [CACHE_NAME];
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    if (!cacheWhitelist.includes(cacheName)) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
 });
